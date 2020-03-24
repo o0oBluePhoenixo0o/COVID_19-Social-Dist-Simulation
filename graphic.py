79abe80090a530f490fd650546fb4778952f14d0
Trung Nguyen <trungnnn1908@gmail.com>
2020-03-24 19:40:49 +0100

    Initialization

---
+++ graphic.py
@@ -0,0 +1,167 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+TEMPLATE = "plotly_white"
+_SUSCEPTIBLE_COLOR = "rgba(230,230,230,.4)"
+_RECOVERED_COLOR = "rgba(180,200,180,.4)"
+
+COLOR_MAP = {
+    "default": "#262730",
+    "pink": "#E22A5B",
+    "purple": "#985FFF",
+    "susceptible": _SUSCEPTIBLE_COLOR,
+    "recovered": _RECOVERED_COLOR,} 
+
+def _set_legends(fig):
+    fig.layout.update(legend=dict(x=-0.1, y=1.2))
+    fig.layout.update(legend_orientation="h")
+
+
+def plot_historical_data(df):
+    fig = px.line(
+        df, x="Date", y="Number", color="Status", template=TEMPLATE
+    )
+    _set_legends(fig)
+
+    return fig
+
+
+def plot_true_versus_confirmed(confirmed, predicted):
+    df = pd.DataFrame(
+        {
+            "Status": ["Confirmed", "Predicted"],
+            "Cases": [confirmed, predicted],
+            "Color": ["b", "r"],
+        }
+    )
+    fig = px.bar(df, x="Status", y="Cases", color="Color", template=TEMPLATE)
+    fig.layout.update(showlegend=False)
+
+    return fig
+
+
+def infection_graph(df, y_max):
+
+    # We cannot explicitly set graph width here, have to do it as injected css: see interface.css
+    fig = go.Figure(layout=dict(template=TEMPLATE))
+
+    susceptible, infected, recovered = (
+        df.loc[df.Status == "Susceptible"],
+        df.loc[df.Status == "Infected"],
+        df.loc[df.Status == "Recovered"],
+    )
+    fig.add_scatter(
+        x=susceptible.Date,
+        y=susceptible.Forecast,
+        fillcolor=COLOR_MAP["susceptible"],
+        fill="tozeroy",
+        mode="lines",
+        line=dict(width=0),
+        name="Uninfected",
+        opacity=0.5,
+    )
+
+    fig.add_scatter(
+        x=recovered.Date,
+        y=recovered.Forecast,
+        fillcolor=COLOR_MAP["recovered"],
+        fill="tozeroy",
+        mode="lines",
+        line=dict(width=0),
+        name="Recovered",
+        opacity=0.5,
+    )
+
+    fig.add_scatter(
+        x=infected.Date,
+        y=infected.Forecast,
+        fillcolor="#FFA000",
+        fill="tozeroy",
+        mode="lines",
+        line=dict(width=0),
+        name="Infected",
+        opacity=0.5,
+    )
+    fig.update_layout(xaxis_rangeslider_visible=True)
+    fig.update_yaxes(range=[0, y_max])
+    fig.layout.update(xaxis_title="Date")
+    _set_legends(fig)
+    return fig
+
+
+def age_segregated_mortality(df):
+    df = df.rename(index={ag: "0-30" for ag in ["0-9", "10-19", "20-29"]}).reset_index()
+    df = pd.melt(df, id_vars="Age Group", var_name="Status", value_name="Forecast")
+    # Add up values for < 30
+    df = (
+        df.groupby(["Age Group", "Status"])
+        .sum()
+        .reset_index(1)
+        .sort_values(by="Status", ascending=False)
+    )
+
+    df['Status'] = df['Status'].apply(lambda x: {'Need Hospitalization': 'Hospitalized'}.get(x, x))
+
+
+    fig = px.bar(
+        df,
+        x=df.index,
+        y="Forecast",
+        color="Status",
+        template=TEMPLATE,
+        opacity=0.7,
+        color_discrete_sequence=["pink", "red"],
+        barmode="group",
+    )
+    fig.layout.update(
+        xaxis_title="",
+        yaxis_title="",
+        font=dict(family="Arial", size=15, color=COLOR_MAP["default"]),
+    )
+    _set_legends(fig)
+    return fig
+
+
+def num_beds_occupancy_comparison_chart(num_beds_available, max_num_beds_needed):
+    """
+    A horizontal bar chart comparing # of beds available compared to 
+    max number number of beds needed
+    """
+    num_beds_available, max_num_beds_needed = (
+        int(num_beds_available),
+        int(max_num_beds_needed),
+    )
+
+    df = pd.DataFrame(
+        {
+            "Label": ["Total Beds ", "Peak Occupancy "],
+            "Value": [num_beds_available, max_num_beds_needed],
+            "Text": [f"{num_beds_available:,}  ", f"{max_num_beds_needed:,}  "],
+            "Color": ["b", "r"],
+        }
+    )
+    fig = px.bar(
+        df,
+        x="Value",
+        y="Label",
+        color="Color",
+        text="Text",
+        orientation="h",
+        opacity=0.7,
+        template=TEMPLATE,
+        height=300,
+    )
+
+    fig.layout.update(
+        showlegend=False,
+        xaxis_title="",
+        xaxis_showticklabels=False,
+        yaxis_title="",
+        yaxis_showticklabels=True,
+        font=dict(family="Arial", size=15, color=COLOR_MAP["default"]),
+    )
+    fig.update_traces(textposition="outside", cliponaxis=False)
+
+    return fig
